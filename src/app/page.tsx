55bbf2371e25051b1264bad4f610281e2106e969
Devi Amaolo <deviamaolo@gmail.com>
2023-05-21 23:58:30 -0300

    create dolar card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
+import '../components/DolarCard'
+import { DolarCard } from '../components/DolarCard'
+
 export default function Home() {
   return (
-    <main className="w-2/5 p-3 bg-white rounded-md flex">
+    <main className="w-2/5 p-3 shadow-md bg-white rounded-md flex">
       <section className="flex-1 flex flex-col justify-center items-center">
-        <h2 className="text-center">Ingrese un monto en pesos $</h2>
+        <h2 className="text-center text-xl text-gray-600 font-bold">Monto en ARS$</h2>
         <input type="number" min="0" max="1000000" className="bg-gray-100 my-4 focus:outline-none h-11 w-40 rounded-md px-2"/>
       </section>
-      <section className="flex-1 h-96 bg-green-700 rounded-md">
-        <h2 className="text-white">Cotizaciones</h2>
+      <section className="flex-1 h-96 bg-green-700 rounded-md flex flex-col gap-5">
+        <h2 className="text-white font-bold text-center mt-2">Cotizaciones</h2>
+        <DolarCard />
+        <DolarCard />
+        <DolarCard />
       </section>
     </main>
   )
---
+++ src/components/DolarCard.tsx
@@ -0,0 +1,19 @@
+export const DolarCard = () => {
+  return (
+    <div>
+        <h3 className="text-center bg-blue-400 mx-6 rounded-t-md text-white">Dólar blue</h3>
+        <div className="flex px-5 bg-gray-100 shadow-md mx-6 rounded-b-md justify-between">
+        <div className="text-center">
+            <h3 className="text-gray-500 font-medium text-sm">COMPRA</h3>
+            <p className="text-green-700 font-bold text-xl">$ 481,00</p>
+            <p className="text-gray-500 text-sm">$ 249,48</p>
+        </div>
+        <div className="text-center">
+            <h3 className="text-gray-500 font-medium text-sm">VENTA</h3>
+            <p className="text-green-700 font-bold text-xl">$ 481,00</p>
+            <p className="text-gray-500 text-sm">$ 249,48</p>
+        </div>
+        </div>
+    </div>
+  )
+}
