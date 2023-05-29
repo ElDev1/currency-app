b444f0a191dd4defc4db69c0be99239d1e4a4191
Devi Amaolo <deviamaolo@gmail.com>
2023-05-29 19:43:14 -0300

    create state logic for ExpenseTracker component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,10 +5,11 @@ import HistoryCard from "@/components/HistoryCard"
 import { useState } from "react"
 
 export default function Page() {
-  const [listOfExpenses, setListOfExpenses] = useState([])
+  const [historyList, setHistoryList] = useState([])
+
   return (
     <section className="bg-white p-9 rounded-md flex gap-5 max-w-2xl w-full">
-      <ExpenseTracker />
+      <ExpenseTracker historyList={historyList} setHistoryList={setHistoryList}/>
       <div className="max-w-5xl w-full">
         <h1>Historial</h1>
         <HistoryCard/>
--- src/components/ExpenseTracker.tsx
+++ src/components/ExpenseTracker.tsx
@@ -1,33 +1,69 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
-export const ExpenseTracker = () => {
-  const [balance, setBalance] = useState({
-        earnings: 0,
-        expenses: 0
-  })
+type historyObj = {
+    text: string 
+    amount: number
+  }
+
+interface Props {
+    historyList: historyObj[]
+    setHistoryList: Function
+}
+
+export const ExpenseTracker = ({historyList, setHistoryList}: Props) => {
+  const [expense, setExpense] = useState(false)
+  const amountButton = useRef<HTMLInputElement>(null)
+  const descriptionButton = useRef<HTMLInputElement>(null) 
+
+  const amounts = historyList.map((elem => elem.amount))
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+  
+  const income = amounts.filter(elem => elem > 0).reduce((acc, price) => (acc += price), 0).toFixed(2)
+  const expenses = (amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) * -1 ).toFixed(2)
+
+   
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(!expense) {
+        const newElem = {
+            text: descriptionButton.current?.value,
+            amount: Number(amountButton.current?.value)
+        }
+        setHistoryList([...historyList, newElem])
+    } else {
+        const newElem = {
+            text: descriptionButton.current?.value,
+            amount: Number(amountButton.current?.value) * -1
+        }
+        setHistoryList([...historyList, newElem])
+    }
+  }
+
+  console.log(historyList)
+
   return (
     <div className="max-w-[250px] w-full">
         <h1 className="text-xl font-bold mb-5">Administrador de gastos</h1>
         <div className="flex w-full text-gray-400 justify-between">
             <p className="">ganancias</p>
-            <p>500</p>
+            <p>{income}</p>
         </div>
         <div className="flex w-full text-gray-400 justify-between">
             <p>gastos</p>
-            <p>300</p>
+            <p>{expenses}</p>
         </div>
         <div className="flex w-full text-gray-600 justify-between">
             <p>tu balance</p>
-            <p className="text-xl">$ 200</p>
+            <p className="text-xl">${total}</p>
         </div>
-        <div className="flex flex-col gap-2 mt-5">
-            <input type="text" required className="bg-gray-200 rounded-md w-full max-w-[250px] p-2 outline-none" placeholder="Compra de supermercado..." />
-            <input type="text" required className="bg-gray-200 rounded-md w-full max-w-[250px] p-2 outline-none" placeholder="00.00"/>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 mt-5">
+            <input type="text" ref={descriptionButton} required className="bg-gray-200 rounded-md w-full max-w-[250px] p-2 outline-none" placeholder="Compra de supermercado..." />
+            <input type="number" ref={amountButton} required className="bg-gray-200 rounded-md w-full max-w-[250px] p-2 outline-none" placeholder="00.00"/>
             <div className="flex gap-2">
-                <button className="px-5 py-1 bg-green-700 rounded-md hover:bg-green-500 text-white">+</button>
-                <button className="px-5 py-1 bg-red-700 rounded-md hover:bg-red-500 text-white">-</button>
+                <button type="submit" onClick={() => setExpense(false)} className="px-5 py-1 bg-green-700 rounded-md hover:bg-green-500 text-white">+</button>
+                <button type="submit" onClick={() => setExpense(true)} className="px-5 py-1 bg-red-700 rounded-md hover:bg-red-500 text-white">-</button>
             </div>
-        </div>
+        </form>
     </div>
   )
 }
